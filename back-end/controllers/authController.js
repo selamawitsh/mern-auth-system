64d755c3608b0f581502e724018a1929bb84d731
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-04-12 11:21:37 +0300

    Add email functionality using Nodemailer for account welcome message

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
 
 export const register= async (req,res)=>{
     console.log(req.body)
@@ -27,7 +28,14 @@ export const register= async (req,res)=>{
             sameSite:process.env.NODE_ENV==='production'? 'none':'strict',
             maxAge:7*24*60*60*1000
         })
-
+        //sending welcome email
+        const mailOptions={
+            from:process.env.SENDER_EMAIL,
+            to:email,
+            subject:'welcome to MERNAUTH',
+            text:`welcome to selam's first project. your account has been created with email id: ${email}`
+        }
+        await transporter.sendMail(mailOptions);
         return res.json({success:true})
 
     } catch (error) {
