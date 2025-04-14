c8e3912cfc9703c07c0aba68ddd48440ba2862ef
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-04-14 21:24:03 +0300

    create context and connect to backend

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,10 +4,15 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
+import { ToastContainer} from 'react-toastify';
+import 'react-roastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div>
+      <ToastContaine>
+        
+      </ToastContaine>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,15 +1,54 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from './../assets/assets';
 // import '../styles/Login.css';
 import {useNavigate} from 'react-router-dom';
-
+import { AppContent } from '../context/AppContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 const Login = () => {
   const navigate = useNavigate()
+
+  const {backendUrl, setIsLoggedin}= useContext(AppContent);
 
   const [state, setState] = useState('Sign Up')
   const [name,setName]= useState("");
   const [email,setEmail]= useState("");
   const [password,setPassword]= useState("");
+
+  const OnsubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      axios.defaults.withCredentials=true;
+      if(state === 'Sign Up'){
+        const {data} =await axios.post(backendUrl + '/api/auth/register' ,
+          {name, email, password}
+         )
+
+         if(data.success){
+          setIsLoggedin(true)
+          navigate('/')
+         }else{
+          toast.error(data.message)
+         }
+      }else{
+        const {data} =await axios.post(backendUrl + '/api/auth/login' ,
+          {email, password}
+         )
+
+         if(data.success){
+          setIsLoggedin(true)
+          navigate('/')
+         }else{
+          toast.error(data.message)
+         }
+
+
+      }
+    } catch (error) {
+      toast.error(data.message)
+    }
+    
+  }
 
   return (
     <div className="login-container">
@@ -17,7 +56,7 @@ const Login = () => {
     <div className="form-container">
       <h2 className="form-title">{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
       <p className="form-subtitle">{state === 'Sign Up' ? 'Create your account' : 'Log in to your account'}</p>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         {state==='Sign Up' && (
           <div className="input-container">
           <img src={assets.person_icon} alt="" className="icon" />
